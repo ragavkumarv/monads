fdfe062c826b738e813019421ae18895a244e8bd
ragavkumarv <ragavkumarv@gmail.com>
2017-10-06 21:57:03 +0530

    chain and fold

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Result =  require('folktale/result')
 const _ = require('lodash/fp');
 const {Ok,Error} = Result;
+console.log(Result)
 
-const tax = (tax, price) => 
+const tax = tax => price => 
     (!_.isNumber(price)) ? Error("Price must be numeric") : Ok(price + (tax * price));
   
   //Returns error or price indluding discount
-  const discount = (dis, price) => {
+  const discount = dis => price => {
     if (!_.isNumber(price)) return Error("Price must be numeric");
   
     if (price < 10) return Error("discount cant be applied for items priced below 10");
@@ -14,13 +15,17 @@ const tax = (tax, price) =>
     return Ok(price - (price * dis));
   };
   
+  const gst = tax(.10);
+  const diwali = discount(.25);
+
   const isError = (e) => e && e.name == 'Error';
   
   const getItemPrice = (item) => Ok(item.price);
   
   //shows total price after tax and discount. Need to handle multiple errors.
-  const showTotalPrice = (item, taxPerc, disount) => {
+  const showTotalPrice1 = (item, taxPerc, disount) => {
     let price = getItemPrice(item);
+ 
     let result = tax(taxPerc, price);
     if (isError(result)) {
       return console.log('Error: ' + result.message);
@@ -32,11 +37,19 @@ const tax = (tax, price) =>
     //display result
     console.log('Total Price: ' + result);
   }
-  
+
+  const log = x => console.log('Error',x)
+  const showTotalPrice = (item) =>
+    getItemPrice(item)
+    .chain(gst)
+    .chain(diwali)
+    .fold(log,console.log)
+    
+
   let tShirt = { name: 't-shirt', price: 11 };
   let pant = { name: 't-shirt', price: '10 dollars' };
   let chips = { name: 't-shirt', price: 5 }; //less than 10 dollars error
   
   showTotalPrice(tShirt) // Total Is: 9.075
-  showTotalPrice(pant)   // Error: Price must be numeric
-  showTotalPrice(chips)  //Error: discount cant be applied for items priced below 10
+//   showTotalPrice(pant)   // Error: Price must be numeric
+//   showTotalPrice(chips)  //Error: discount cant be applied for items priced below 10
