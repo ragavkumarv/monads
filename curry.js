fe8cc7bdff3c0388393db9942985ffb171e565a6
ragavkumarv <ragavkumarv@gmail.com>
2017-10-07 12:09:44 +0530

    ans

--- curry.js
+++ curry.js
@@ -13,13 +13,23 @@ inc(100);
 
 /*======== Challenge 1 ======*/
 //Wirte a double function only with R.multiply R.map to  [1,3,6]
+const double = R.map(R.multiply(2))
 
+log(double([1,3,6]))
 
 
 /*======== Challenge 2 ======*/
 const greater = (a,b) => a > b ? a : b;
 //Wirte a max function only with R.reduce or R.filter R.map to  [-1,-1000,-3]
 
+const max = R.reduce(greater, -Infinity);
+
+log(max([-1,-1000,-3]))
+
 
 /*======== Challenge 3 ======*/
 // Write a function to double and find the max
+
+const doubleMax = R.compose(max,double)
+
+log(doubleMax([-1,200,-3]))
