c320d417fad5a7de3cddd355f28590ea9320acfd
ragavkumarv <ragavkumarv@gmail.com>
2017-10-07 14:11:06 +0530

    challenge starts

--- monad.js
+++ monad.js
@@ -5,7 +5,7 @@ const Box = x =>
 ({
 	map:f	=> Box(f(x)),
 	fold:f => f(x),
-    Inspect: () => `Box(${x})` 
+    inspect: () => `Box(${x})` 
 })
 
 
@@ -13,6 +13,29 @@ const strUpper =  str =>
 Box(str)
 .map(R.toUpper)
 .map(str=>str.length)
-.Inspect()
+.inspect()
 
 console.log(strUpper('Ragav'))
+
+//Old name Either new Name Result
+const Ok = x => 
+({
+    map:f	=> Ok(f(x)),
+    fold:(f,g) => g(x),
+    inspect: () => `Ok(${x})` 
+})
+
+const Error = x => 
+({
+    map:f	=> Error(x),
+    fold: (f,g)=> f(x),
+    inspect: () => `Error(${x})` 
+})
+
+const result = 
+    Error(2)    
+    .map(x => x + 1)
+    .map(x => x/2)
+    .fold(x=>'error', x => x)
+
+log(result)    
